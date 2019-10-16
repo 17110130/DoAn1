0879749a2f3c5beb2d7c36f9dd06f80d0061b69b
Thang99999999 <43449125+Thang99999999@users.noreply.github.com>
2019-10-16 13:16:41 +0700

    @thang99999999
    apdate

---
+++ Doan1/Doan1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Doan1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="admin")
+            {
+                if(textBox2.Text=="admin")
+                {
+                    MessageBox.Show("login success");
+                    Form a = new Form2();
+                    this.Hide();
+                    a.ShowDialog();
+                    Application.Exit();
+                }
+                else
+                {
+                    MessageBox.Show("password!!!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("username!!!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Doan1/Doan1/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Doan1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(@"C:\Users\Admin\source\repos\Doan1\Doan1\bin\Debug\dsnuoc.txt",FileMode.Open);
+            StreamReader sr = new StreamReader(fs, Encoding.UTF8);
+            textBox2.Text = sr.ReadToEnd();
+            sr.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(@"C:\Users\Admin\source\repos\Doan1\Doan1\bin\Debug\gia.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs, Encoding.UTF8);
+            textBox2.Text = sr.ReadToEnd();
+            sr.Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(@"C:\Users\Admin\source\repos\Doan1\Doan1\bin\Debug\khuyenmai.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs, Encoding.UTF8);
+            textBox2.Text = sr.ReadToEnd();
+            sr.Close();
+        }
+    }
+}
