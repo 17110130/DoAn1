49491db774a90b763dc08da626300f746cc98175
Thang99999999 <43449125+Thang99999999@users.noreply.github.com>
2019-10-22 09:10:52 +0700

    @thang99999999
    ahihi

--- Doan1/Doan1/Form2.Designer.cs
+++ Doan1/Doan1/Form2.Designer.cs
@@ -29,34 +29,21 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(88, 144);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(198, 22);
-            this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(340, 118);
+            this.textBox2.Location = new System.Drawing.Point(292, 119);
             this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(314, 320);
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(469, 320);
             this.textBox2.TabIndex = 1;
             // 
             // button1
@@ -102,48 +89,6 @@
             this.button4.Text = "Order";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(292, 119);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(42, 319);
-            this.textBox3.TabIndex = 6;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(660, 119);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(101, 319);
-            this.textBox4.TabIndex = 7;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(94, 118);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 17);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "number";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(94, 168);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(25, 17);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "bill";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(88, 188);
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(198, 250);
-            this.textBox5.TabIndex = 10;
-            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -151,17 +96,11 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -173,17 +112,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox5;
     }
 }
--- Doan1/Doan1/Form2.cs
+++ Doan1/Doan1/Form2.cs
@@ -20,51 +20,26 @@ namespace Doan1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            FileStream fs = new FileStream(@"C:\Users\Admin\OneDrive\Máy tính\DoAn1\Doan1\Doan1\bin\Debug\dsnuoc.txt", FileMode.Open);
-            FileStream fs1 = new FileStream(@"C:\Users\Admin\OneDrive\Máy tính\DoAn1\Doan1\Doan1\bin\Debug\stt.txt", FileMode.Open);
-            FileStream fs2 = new FileStream(@"C:\Users\Admin\OneDrive\Máy tính\DoAn1\Doan1\Doan1\bin\Debug\sttd.txt", FileMode.Open);
-            StreamReader sr = new StreamReader(fs, Encoding.UTF8);
-            StreamReader sr1 = new StreamReader(fs1, Encoding.UTF8);
-            StreamReader sr2 = new StreamReader(fs2, Encoding.UTF8);
-            textBox2.Text = sr.ReadToEnd();
-            textBox3.Text = sr1.ReadToEnd();
-            textBox4.Text = sr2.ReadToEnd();
-            sr.Close();
-            sr1.Close();
-            sr2.Close();
+            FileStream fs = new FileStream(@"C:\Users\Admin\OneDrive\Máy tính\DoAn1\Doan1\Doan1\bin\Debug\dsnuoc.txt", FileMode.Open);                     
+            StreamReader sr = new StreamReader(fs, Encoding.UTF8);                       
+            textBox2.Text = sr.ReadToEnd();                        
+            sr.Close();                       
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            FileStream fs = new FileStream(@"C:\Users\Admin\OneDrive\Máy tính\DoAn1\Doan1\Doan1\bin\Debug\gia.txt", FileMode.Open);
-            FileStream fs1 = new FileStream(@"C:\Users\Admin\OneDrive\Máy tính\DoAn1\Doan1\Doan1\bin\Debug\stt.txt", FileMode.Open);
-            
-            StreamReader sr = new StreamReader(fs, Encoding.UTF8);
-            StreamReader sr1 = new StreamReader(fs1, Encoding.UTF8);
-            
+            FileStream fs = new FileStream(@"C:\Users\Admin\OneDrive\Máy tính\DoAn1\Doan1\Doan1\bin\Debug\gia.txt", FileMode.Open);                        
+            StreamReader sr = new StreamReader(fs, Encoding.UTF8);          
             textBox2.Text = sr.ReadToEnd();
-            textBox3.Text = sr1.ReadToEnd();
-            
-            sr.Close();
-            sr1.Close();
-            
-
+            sr.Close();                      
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            FileStream fs = new FileStream(@"C:\Users\Admin\OneDrive\Máy tính\DoAn1\Doan1\Doan1\bin\Debug\khuyenmai.txt", FileMode.Open);
-            FileStream fs1 = new FileStream(@"C:\Users\Admin\OneDrive\Máy tính\DoAn1\Doan1\Doan1\bin\Debug\sttkm.txt", FileMode.Open);
-            FileStream fs2 = new FileStream(@"C:\Users\Admin\OneDrive\Máy tính\DoAn1\Doan1\Doan1\bin\Debug\sttkms.txt", FileMode.Open);
-            StreamReader sr = new StreamReader(fs, Encoding.UTF8);
-            StreamReader sr1 = new StreamReader(fs1, Encoding.UTF8);
-            StreamReader sr2 = new StreamReader(fs2, Encoding.UTF8);
-            textBox2.Text = sr.ReadToEnd();
-            textBox3.Text = sr1.ReadToEnd();
-            textBox4.Text = sr2.ReadToEnd();
-            sr.Close();
-            sr1.Close();
-            sr2.Close();
+            FileStream fs = new FileStream(@"C:\Users\Admin\OneDrive\Máy tính\DoAn1\Doan1\Doan1\bin\Debug\khuyenmai.txt", FileMode.Open);                       
+            StreamReader sr = new StreamReader(fs, Encoding.UTF8);                     
+            textBox2.Text = sr.ReadToEnd();         
+            sr.Close(); 
         }
 
         private void Form2_Load(object sender, EventArgs e)
