f15a901c2044c49c892cf969b238dc4168d4f0e6
17110130 <you@example.com>
2019-11-06 20:42:15 +0700

    Add New

--- Doan1/Doan1/frmHoaDon.cs
+++ Doan1/Doan1/frmHoaDon.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,24 @@ namespace Doan1
 
         private void btnExportFile_Click(object sender, EventArgs e)
         {
+            FileStream fw = null;
+            string msg = "";
+            byte[] msgByte = null;
 
+            fw = new FileStream("Bill.txt", FileMode.Create);
+
+            msgByte = Encoding.Default.GetBytes(msg);
+            fw.Write(msgByte, 0, msgByte.Length);
+            msg = "";
+
+            msg += lblNameCusBill.Text + "\r\n" + txtBill.Text + "\r\n" + lblTotalBill.Text + "\r\n";
+
+            msgByte = Encoding.Default.GetBytes(msg);
+            fw.Write(msgByte, 0, msgByte.Length);
+
+            if (fw != null) fw.Close();
+
+            MessageBox.Show("The file Bill.txt have created!");
         }
     }
 }
